d8924e0318fa469e5521a6fdf2d94fbe97db5222
lansdell <ben.lansdell@gmail.com>
2019-11-14 17:50:07 -0500

    Update scripts

---
+++ cifar10_bpnoise_runs.sh
@@ -0,0 +1,14 @@
+#BP w noisy activations
+python3 ./cifar10_conv_bp_noise.py --gpu 1 --name cifar10_conv_bp_noise_run01 --save 1
+python3 ./cifar10_conv_bp_noise.py --gpu 1 --name cifar10_conv_bp_noise_run02 --save 1
+python3 ./cifar10_conv_bp_noise.py --gpu 1 --name cifar10_conv_bp_noise_run03 --save 1
+
+#DFA w noisy activations
+#python3 ./cifar10_conv_noise.py --gpu 2 --name cifar10_conv_dfa_noise_run01 --save 1
+#python3 ./cifar10_conv_noise.py --gpu 2 --name cifar10_conv_dfa_noise_run02 --save 1
+#python3 ./cifar10_conv_noise.py --gpu 2 --name cifar10_conv_dfa_noise_run03 --save 1
+
+#Synthetic gradient runs
+#python3 ./cifar10_conv_sg.py --gpu 3 --name cifar10_conv_sg_run01 --save 1
+#python3 ./cifar10_conv_sg.py --gpu 3 --name cifar10_conv_sg_run02 --save 1
+#python3 ./cifar10_conv_sg.py --gpu 3 --name cifar10_conv_sg_run03 --save 1
